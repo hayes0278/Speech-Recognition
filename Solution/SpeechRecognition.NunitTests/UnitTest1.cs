17fb069028a9b262e0b8b8e3ee05b7df8e60eef6
Marshall Hayes <hayes0278@users.noreply.github.com>
2025-10-06 17:04:40 -0500

    Added initial scaffolding.

---
+++ Solution/SpeechRecognition.NunitTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace SpeechRecognition.NunitTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
